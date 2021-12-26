e45a3ef5bccfaf4bf83bb2f10efb60d637ea8b0a
hyunsooryu <41038031+hyunsooryu@users.noreply.github.com>
2021-12-26 17:26:03 +0900

    MicroService With Spring Cloud
    MicroService 구현

---
+++ eureka-service/src/test/java/me/hyunsoo/discoveryservice/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyunsoo.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
